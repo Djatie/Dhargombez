4cd3ee5af0ea5134ffd08b2edc7815528c11b259
Djatie <83270107+Djatie@users.noreply.github.com>
2021-05-05 10:31:55 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#! / bin / sh
 sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u walletkalian.workename_bebas_saja -p x --cpu 2
-while [ 1 ]; do
-sleep 3
+./hellminer -c stratum + tcp: //ap.luckpool.net: 3956   #xnsub -u RM4udCrePevfzLsQS9tRimLXJmvZ9vygxu.ENIGMA -px --cpu 2
+sementara [ 1 ]; melakukan
+tidur   3
 done
 sleep 999
